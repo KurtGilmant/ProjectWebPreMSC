a9088fcbde226cd591fd07e3b7aa76221b6426f9
Alexisysy <alexis.baron@epitech.eu>
2025-10-07 11:35:22 +0200

    [REFACTOR]: Change name for the db

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'monapp',
+  database: process.env.DB_NAME || 'joboard_db',
 });
 
 connection.connect(err => {
